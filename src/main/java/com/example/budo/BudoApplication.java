8a20cb8dc0656b819b313fcef2a20adb83814bcf
supergillie <peter.krooks@gmail.com>
2021-11-20 19:33:09 +0100

    first commit

---
+++ src/main/java/com/example/budo/BudoApplication.java
@@ -0,0 +1,50 @@
+package com.example.budo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+@SpringBootApplication
+public class BudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudoApplication.class, args);
+	}
+
+	@GetMapping("/apa")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+	@GetMapping("/beppe")
+	public String rollo() {
+		return "!Greetings from Spring Rolle!";
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
+
+
+
+
+
+}
---
+++ src/test/java/com/example/budo/BudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.budo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
