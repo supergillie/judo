e2b53ace9f42563e41d2643d2edd4744b123de41
supergillie <peter.krooks@gmail.com>
2021-11-21 09:21:27 +0100

    testfix2

--- src/test/java/com/example/budo/BudoApplicationTests.java
+++ src/test/java/com/example/budo/BudoApplicationTests.java
@@ -2,9 +2,6 @@ package com.example.budo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import java.io.IOException;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class BudoApplicationTests {
