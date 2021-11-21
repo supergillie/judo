0ecc04c2138a130759a703a393fdd25b97a42175
supergillie <peter.krooks@gmail.com>
2021-11-21 12:05:14 +0100

    links links

--- public/hello.js
+++ public/hello.js
@@ -1,6 +1,6 @@
 angular.module('demo', [])
 .controller('Hello', function($scope, $http) {
-    $https.get('https://budonord.herokuapp.com/strat').
+    $http.get('https://budonord.herokuapp.com/strat').
         then(function(response) {
             $scope.greeting = response.data;
         });
