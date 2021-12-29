0307d245782b61ca5fc1fe229a1b78a6d9dce867
supergillie <peter.krooks@gmail.com>
2021-12-30 00:48:54 +0100

    peter trst

--- public/hello.js
+++
@@ -1,7 +0,0 @@
-angular.module('demo', [])
-.controller('Hello', function($scope, $http) {
-    $http.get('http://budonord.herokuapp.com/strat', { mode: 'no-cors'}).
-        then(function(response) {
-            $scope.greeting = 'response.data';
-        });
-});
